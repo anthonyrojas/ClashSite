1a132b1189089abc213dedae286809a2bf73f55b
anthonyrojas <anthonyrojas095@gmail.com>
2018-01-28 01:33:49 -0800

    about page progress

--- controllers/clanController.js
+++ controllers/clanController.js
@@ -1,99 +1,75 @@
 "use strict"
 
 const request = require('request');
+const axios = require('axios');
 const config = require('../config');
+
+const configuration = {
+    headers: {
+        auth: config.authKey
+    }
+};
 
 exports.getClan = (req, res, next)=>{
     var clanTag = req.params.tag;
-    var options = {
-        url: config.host + '/clan/' + clanTag,
-        headers: {
-            auth: config.authKey
-        }
-    };
-    request(options, (error, response, body)=>{
-        if(error){
-            throw new Error('failed to retrieve data, API must be down');
-        }
-        else if(!error && (response.statusCode === 200 || response.statusCode === 304)){
-            //res.json(JSON.parse(body));
-            res.locals.clan = JSON.parse(body);
-            next();
-        }
-        else{
-            //res.json(JSON.parse(body));
-            next();
-        }
+
+    axios.get(config.host + '/clan/' + clanTag, configuration)
+    .then(response => {
+        res.locals.clan = response.data;
+        next();
+    })
+    .catch(error=>{
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        console.log(error.response.body);
+        next();
+        /*TODO: error handling*/
     });
 };
 
 exports.getPlayer = (req, res, next)=>{
     var playerTag = req.params.tag;
-    var options = {
-        url: config.host + '/player/' + playerTag + '?exclude=battles,achievements,cards',
-        headers: {
-            auth: config.authKey
-        }
-    };
-    request(options, (error, response, body)=>{
-        if(error){
-            throw new Error('failed to retrieve data, API must be down');
-        }
-        else if(!error && (response.statusCode === 200 || response.statusCode === 304)){
-            //res.json(JSON.parse(body));
-            //console.log(JSON.parse(body));
-            res.locals.player = JSON.parse(body);
-            next();
-        }
-        else{
-            //res.json(JSON.parse(body));
-            //res.json(JSON.parse(body));
-            next();
-        }
+    axios.get(config.host + '/player/' + playerTag, configuration)
+    .then(response=>{
+        res.locals.player = response.data;
+        next();
+    })
+    .catch(error=>{
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        console.log(error.response.body);
+        next();
+        /*TODO: better error handling*/
     });
 };
 
 exports.getClanHistory = (req, res, next)=>{
     var clanTag = req.params.tag;
-    var options = {
-        url: config.host + '/clan/' + clanTag + '/history',
-        headers: {
-            auth: config.authKey
-        }
-    };
-    request(options, (error, response, body)=>{
-        if(error){
-            throw new Error('failed to retrieve data, API must be down');
-        }
-        else if(!error && (response.statusCode === 200 || response.statusCode === 304)){
-            res.json(JSON.parse(body));
-        }
-        else{
-            res.json(JSON.parse(body));
-        }
+    axios.get(config.host + '/clan/' + clanTag + '/history', configuration)
+    .then(response=>{
+        res.locals.clanHistory = response.data;
+        next();
+    })
+    .catch(error=>{
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        console.log(error.response.body);
+        next();
     });
 };
 
 exports.getNormies = (req, res, next)=>{
-    const options = {
-        url: config.host + '/clan/' + config.normiesTag,
-        headers: {
-            auth: config.authKey
-        }
-    };
-    request(options, (error, response, body)=>{
-        if(error){
-            throw new Error('failed to retrieve data. API must be down');
-        }
-        else if(!error && (response.statusCode === 200 || response.statusCode === 304)){
-            var clanData = JSON.parse(body);
-            //console.log(clanData);
-            //res.locals.normies = clanData.members;
-            res.locals.normies = clanData;
-            next();
-        }
-        else{
-            next();
-        }
+    axios.get(config.host + '/clan/' + config.normiesTag, configuration)
+    .then(response=>{
+        res.locals.normies = response.data;
+        next();
+    })
+    .catch(error=>{
+        console.log(error.response.status);
+        console.log(error.response.statusText);
+        console.log(error.response.body);
+        res.send(error.response.body);
+        next();
+        /*TODO: better error handling*/
     });
 };
