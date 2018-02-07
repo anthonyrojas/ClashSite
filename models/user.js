06d06725a269801f3a994b29c2ca3b90f0bc4dd8
anthonyrojas <anthonyrojas095@gmail.com>
2018-02-07 00:18:48 -0800

    battle history page

--- models/user.js
+++ models/user.js
@@ -12,8 +12,24 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    playerTag: {
+        type: String,
+        required: true  
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
     }
 });
+
+UserSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
 
 
 module.exports = mongoose.model('User', UserSchema);
