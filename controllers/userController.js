2c08b3415b826657bb422035fe69b58e39d33d45
anthonyrojas <anthonyrojas095@gmail.com>
2018-02-09 00:10:50 -0800

    user registration works

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,22 +27,23 @@ exports.register = (req, res, next)=>{
     if(!playerTagEntry){
         return res.status(422).send({error: 'You must enter a player tag.'});
     }
-    //obtain the user name for the new user
-    axios.get(config.host + '/player/' + playerTagEntry.toString().trim() + '?keys=tag,name,clan', crapiConfig)
-    .then(response=>{
-        if(response.data.name === null || response.data.name === "" || response.data.name == undefined ){
-            return res.status(422).send({error: 'Player information not found. Invalid tag.'});
+
+    //check if a user with the player tag exists
+    User.findOne({playerTagEntry}, (err, existingUser)=>{
+        if(err){
+            return res.status(500).send({error: 'Error connecting to the database.'});
         }
-        const usernameEntry = response.data.name;
-        //check if a user with the player tag exists
-        User.findOne({playerTagEntry}, (err, existingUser)=>{
-            if(err){
-                return res.status(500).send({error: 'Error connecting to the database.'});
+        //return error if user exists
+        if(existingUser){
+            return res.status(422).send({error: 'The player tag you have entered is already in use.'});
+        }
+        //obtain the user name for the new user if the player tag does not exist
+        axios.get(config.host + '/player/' + playerTagEntry + '?keys=tag,name,clan', crapiConfig)
+        .then(response=>{
+            if(response.data.name === null || response.data.name === "" || response.data.name == undefined ){
+                return res.status(422).send({error: 'Player information not found. Invalid tag.'});
             }
-            //return error if user exists
-            if(existingUser){
-                return res.status(422).send({error: 'The player tag you have entered is already in use.'});
-            }
+            const usernameEntry = response.data.name;
             //create and save the user if it does not exist
             var newUser = new User({
                 email: emailEntry,
@@ -51,6 +52,7 @@ exports.register = (req, res, next)=>{
                 playerTag: playerTagEntry
             });
             //console.log(newUser);
+            //save the new user in the database
             newUser.save((err, user)=>{
                 if(err){
                     return res.status(400).send({
@@ -61,22 +63,22 @@ exports.register = (req, res, next)=>{
                     res.json(user);
                 }
             });
+        })
+        .catch(error=>{
+            const errStatus = error.response.status;
+            if(errStatus === 404 || errStatus === 400){
+                return res.status(422).send({error: 'Player information not found. Invalid tag. Input a valid player tag.'});
+            }
+            else if(errStatus === 503){
+                return res.status(422).send({error: 'The service from which we draw data is currently down. Please try to register again later.'});
+            }
+            else if(errStatus === 429){
+                return res.status(422).send({error: 'Cr-api.com has throttled the amount of requests that can be made per 2 minutes. Seems the limit has been exceeded. Please wait for the cooldown and you may complete registration shortly.'});
+            }
+            else{
+                return res.status(422).send({error: 'Player information not found or could not be obtained. Input a valid player tag.'});
+            }
         });
-    })
-    .catch(error=>{
-        const errStatus = error.response.status;
-        if(errStatus === 404 || errStatus === 400){
-            return res.status(422).send({error: 'Player information not found. Invalid tag. Input a valid player tag.'});
-        }
-        else if(errStatus === 503){
-            return res.status(422).send({error: 'The service from which we draw data is currently down. Please try to register again later.'});
-        }
-        else if(errStatus === 429){
-            return res.status(422).send({error: 'Cr-api.com has throttled the amount of requests that can be made per 2 minutes. Seems the limit has been exceeded. Please wait for the cooldown and you may complete registration shortly.'});
-        }
-        else{
-            return res.status(422).send({error: 'Player information not found or could not be obtained. Input a valid player tag.'});
-        }
     });
 };
 
@@ -103,4 +105,3 @@ exports.loginRequired = (req, res, next)=>{
         return res.status(401).json({message: 'Unauthorized user!'});
     }
 };
-
